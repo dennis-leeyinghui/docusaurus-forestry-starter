9215df6e15e662a025fb505b97e04c16af7ee6ef
jarl-lee <50139939+pinky-pomelo@users.noreply.github.com>
2021-04-18 13:31:14 -0300

    removed navbar title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ module.exports = {
   projectName: 'docusaurus-forestry-starter', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'modernify.ca',
+      title: '',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
