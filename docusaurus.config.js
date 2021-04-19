4c1932e1900c582630cba9debe2762ff6586aafe
jarl-lee <50139939+pinky-pomelo@users.noreply.github.com>
2021-04-18 23:27:52 -0300

    css

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,7 +39,7 @@ module.exports = {
 		 position: 'left'
 		},
         {
-          href: 'contact/',
+          to: 'contact/',
           label: 'Contact',
           position: 'left',
         },
