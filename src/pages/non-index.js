03ca0c03ee79c57c70a71c06bece9e8dcc510974
jarl-lee <50139939+pinky-pomelo@users.noreply.github.com>
2021-04-18 23:55:02 -0300

    css

---
+++ src/pages/non-index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import classnames from 'classnames';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
+
+// Portfolio Grid End *********
+
+function Home() {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="We help Atlantic Canadian businesses look better online.">
+      <header className={classnames('hero hero--primary', styles.heroBanner)}>
+      </header>
+
+      <main>
+        <div><h3> QQQQQ </h3></div>
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;
